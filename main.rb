3d9396886e2f4edc71187655e00b2b112a7a7a23
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-12-10 15:30:55 +0300

    change server settings

--- main.rb
+++ main.rb
@@ -12,6 +12,7 @@ $matchmaker = Matchmaker.new
 set :server, 'thin'
 set :static_cache_control, [:no_cache]
 set :bind, $settings_hash[:ip_bind]
+set :port, $settings_hash[:port] || 4567
 
 configure do
   enable :sessions
